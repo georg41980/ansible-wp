c85c099ae6e1bc15ce580d0c5c30276f93f74120
georg41980 <ylesrvny@duck.com>
2024-01-25 17:07:50 +0100

    docker

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -2,3 +2,5 @@
 ansible-playbook -i pi, --user georg playbooks/install_docker.yml
 
 ansible-playbook -i pi, --user georg playbooks/apache_demo.yml
+
+ansible-playbook -i pi, --user georg playbooks/docker_https_portal.yml 
