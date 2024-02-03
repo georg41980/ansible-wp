587eaaf849f7d880a135fd7a4faaac8a85dfd99b
georg41980 <ylesrvny@duck.com>
2024-02-03 12:47:12 +0100

    test

--- ansible/demo01/run_ansible.sh
+++ ansible/demo01/run_ansible.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ansible-playbook -i pi, --user georg install_docker.yaml
+ansible-playbook apt_upgrade.yaml
+ansible-playbook change_ssh_port.yaml
+ansible-playbook docker_create_proxynet.yaml
+ansible-playbook install_docker.yaml
+ansible-playbook docker_ghost.yaml
+ansible-playbook docker_pihole.yaml
+ansible-playbook docker_pmnginx.yaml
+ansible-playbook docker_s-pdf.yaml
+ansible-playbook docker_wordpress.yaml
